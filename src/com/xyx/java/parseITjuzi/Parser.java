91101a91eca5249565e60e0c08b948e1992d8027
rainshang <rainshanghsu@qq.com>
2015-11-09 18:33:52 +0800

    下班提交

--- src/com/xyx/java/parseITjuzi/Parser.java
+++ src/com/xyx/java/parseITjuzi/Parser.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 public class Parser {
 	private final static String COOKIE = "BAIDU_DUP_lcr=https://www.baidu.com/link?url=SACPyz-DQJIByrfzGenEWE-6VOfkkye-PeE1oWWD9iK&wd=&eqid=cda04c5600002e6e00000003564003d3; gr_user_id=68d974ae-b3c0-4176-9623-76e253ba672d; cisession=a%3A4%3A%7Bs%3A10%3A%22session_id%22%3Bs%3A32%3A%222fbda95b63aaac150bcc7b5e47b01351%22%3Bs%3A10%3A%22ip_address%22%3Bs%3A14%3A%22123.115.64.135%22%3Bs%3A10%3A%22user_agent%22%3Bs%3A120%3A%22Mozilla%2F5.0+%28Macintosh%3B+Intel+Mac+OS+X+10_11_1%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F46.0.2490.80+Safari%2F537.36%22%3Bs%3A13%3A%22last_activity%22%3Bi%3A1447052247%3B%7D82476f1c6fe3624b251bc054c4f8c0a8; front_identity=1252394237%40qq.com; front_remember_code=8b4c03831e15efc87c8ce255907b16d371dcda31; Hm_lvt_1c587ad486cdb6b962e94fc2002edf89=1447035866; Hm_lpvt_1c587ad486cdb6b962e94fc2002edf89=1447052359; gr_session_id=8bf6d5ca-9ffd-4759-88bc-0db9a314a993";
@@ -38,9 +39,11 @@ public class Parser {
 	private void parseITjuzi(String formatUrl) {
 		int currentPageIndex = 1;
 		int pageCount = Integer.MAX_VALUE;
-		while (currentPageIndex <= pageCount) {
-			Document document = PARSER.httpGet2Document(String.format(formatUrl, currentPageIndex));
-		}
+		// while (currentPageIndex <= pageCount) {
+		Document document = PARSER.httpGet2Document(String.format(formatUrl, currentPageIndex));
+		Element element = document.select("div[class=\"normal-box-no-pad follow-area\"]").get(0);
+		System.out.println(element);
+		// }
 	}
 
 	private Document httpGet2Document(String getUrl) {
--- src/com/xyx/java/parseITjuzi/Project.java
+++ src/com/xyx/java/parseITjuzi/Project.java
@@ -1,5 +1,92 @@
 package com.xyx.java.parseITjuzi;
 
+import java.text.SimpleDateFormat;
+
 public class Project {
+	private final static SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy.MM");
+
+	public int projectId;
+	public String projectName;
+	public String projectBrief;
+	public String projectCompany;
+	public String projectIndustry;
+	public String projectIntro;
+	public String projectStory;
+	public String projectWebsite;
+	public int[] projectAddressCodes;
+
+	public String projectFundPhase;
+	public String projectFundValue;
+	public String projectFundUnit;
+
+	public PastFinance[] pastFinances;
+	public PastInvestor[] pastInvestors;
+	public Person[] founders;
+	public Person[] employees;
+
+	/**
+	 * 融资经历
+	 * 
+	 * @author rainshang
+	 * 
+	 */
+	public class PastFinance {
+		public long dateL;
+		public String phase;
+		public String financeAmount;
+		public String financeAmountUnit;
+		public String valuation;
+		public String valuationUnit;
+		public String[] participants;
+	}
+
+	/**
+	 * 过往资方
+	 * 
+	 * @author rainshang
+	 * 
+	 */
+	public class PastInvestor {
+		public String name;
+		public String brief;
+
+	}
+
+	/**
+	 * 人员信息
+	 * 
+	 * @author rainshang
+	 * 
+	 */
+	public class Person {
+		public int id;
+		public String name;
+		public String nickName;
+		public String type;
+		public String intro;
+
+		// https://rong.36kr.com/api/user/53366/basic
+		public String allIntro;
+		public String[] tags;
+
+		public PersonCompanyExp[] companyExps;// https://rong.36kr.com/api/user/53366/company
+		public PersonWorkExp[] workExps;// https://rong.36kr.com/api/user/53366/work
+
+		public class PersonCompanyExp {
+			public String groupName;
+			public String brief;
+			public String positionString;
+			public long startDateL, endDateL;
+
+		}
+
+		public class PersonWorkExp {
+			public String groupName;
+			public String positionString;
+			public String positionDetail;
+			public long startDateL, endDateL;
+
+		}
+	}
 
 }
